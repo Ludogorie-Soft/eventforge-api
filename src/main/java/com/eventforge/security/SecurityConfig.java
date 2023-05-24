e23e9bb448e4c246522322755060be8bf3acfc05
Valentin Vergilov <valyo01996@gmail.com>
2023-05-24 17:25:56 +0300

    Security set up (no JWT token).

--- src/main/java/com/eventforge/security/SecurityConfig.java
+++ src/main/java/com/eventforge/security/SecurityConfig.java
@@ -1,7 +1,5 @@
 package com.eventforge.security;
 
-import com.eventforge.enums.Role;
-import com.eventforge.security.jwt.JWTAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,35 +9,33 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-    private final JWTAuthenticationFilter authenticationFilter;
+
 
     private final MyUserDetailsService userDetailsService;
 
-    private static final String[] SECURED_URLs = {"/admin/**" ,"/organisation/**"};
-    private static final String[] UNSECURED_URLs = {"/registration" , "/menu/**", "/login" ,"/authenticate"};
+    private static final String[] ADMIN_URLs = {"/admin/**"};
+    private static final String[] ORGANISATION_URLs = {"/organisation/**"};
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.csrf().disable()
-                .authorizeHttpRequests().requestMatchers(UNSECURED_URLs).permitAll()
-                .and()
-                .authorizeHttpRequests().requestMatchers(SECURED_URLs).hasAuthority(Role.ADMIN.toString())
-                .anyRequest().authenticated()
-                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-                .authenticationProvider(authenticationProvider()).addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class).build();
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        return http
+                .csrf().disable()
+                .authorizeHttpRequests().requestMatchers(ADMIN_URLs).hasAuthority("ADMIN")
+                .and().authorizeHttpRequests().requestMatchers(ORGANISATION_URLs).hasAuthority("ORGANISATION")
+                .anyRequest().permitAll().and().formLogin()
+                .loginPage("/login").defaultSuccessUrl("/menu" , true).permitAll().and().logout().logoutSuccessUrl("/menu").
+                permitAll().and().build();
     }
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfiguration) throws Exception {
