aa3a809dccced37f8a4a1eb350300a7ded668cf2
kichukovrd <kichukovrd@gmail.com>
2023-06-03 17:29:49 +0300

    added some tests into OrganisationServiceTest and UserServiceTest

---
+++ src/test/java/com/eventforge/service/OrganisationServiceTest.java
@@ -0,0 +1,27 @@
+package com.eventforge.service;
+
+import com.eventforge.model.Organisation;
+import com.eventforge.repository.OrganisationRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class OrganisationServiceTest {
+    @Mock
+    private OrganisationRepository organizationRepository;
+    @InjectMocks
+    private OrganisationService organisationService;
+
+    @Test
+    void testSaveOrganisationInDb() {
+        Organisation organisation = new Organisation();
+
+        organisationService.saveOrganisationInDb(organisation);
+        verify(organizationRepository).save(organisation);
+    }
+}
