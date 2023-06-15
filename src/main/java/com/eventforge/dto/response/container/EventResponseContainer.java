be7d051e495199f7ed4e2af3ae564a39ff355167
Valentin Vergilov <valyo01996@gmail.com>
2023-06-15 11:55:37 +0300

    a

--- src/main/java/com/eventforge/dto/response/container/EventResponseContainer.java
+++ src/main/java/com/eventforge/dto/response/container/EventResponseContainer.java
@@ -2,14 +2,30 @@ package com.eventforge.dto.response.container;
 
 import com.eventforge.dto.response.OneTimeEventResponse;
 import com.eventforge.dto.response.RecurrenceEventResponse;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.List;
 @Data
-@AllArgsConstructor
 public class EventResponseContainer {
-    private List<OneTimeEventResponse> oneTimeEvents;
-    private List<RecurrenceEventResponse> recurrenceEvents;
+    private List<OneTimeEventResponse> oneTimeEvents = new ArrayList<>();
+    private List<RecurrenceEventResponse> recurrenceEvents = new ArrayList<>();
 
+
+    public EventResponseContainer(List<OneTimeEventResponse> oneTimeEvents , List<RecurrenceEventResponse> recurrenceEvents){
+        addOneTimeEvents(oneTimeEvents);
+        addRecurrenceEvents(recurrenceEvents);
+    }
+
+    private void addOneTimeEvents(List<OneTimeEventResponse> oneTimeEvents){
+        if(oneTimeEvents!=null){
+            this.oneTimeEvents.addAll(oneTimeEvents);
+        }
+    }
+
+    private void addRecurrenceEvents(List<RecurrenceEventResponse> recurrenceEvents){
+        if(recurrenceEvents!=null){
+            this.recurrenceEvents.addAll(recurrenceEvents);
+        }
+    }
 }
