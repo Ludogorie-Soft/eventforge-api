2a6015fac0c8146b72d33ddd8b42f33c887f8352
Valyo96 <107226915+Valyo96@users.noreply.github.com>
2023-06-27 16:32:28 +0300

    FIX BUG (#16)
    fixed the security bug and added few exceptions

---
+++ src/main/java/com/eventforge/annotation/PasswordsMustMatch.java
@@ -0,0 +1,20 @@
+package com.eventforge.annotation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = PasswordsMustMatchValidator.class)
+public @interface PasswordsMustMatch {
+    String message() default "Паролите не съвпадат!";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
